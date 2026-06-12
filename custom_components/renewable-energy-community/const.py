f3c03b7922fb755ea2a0923ee84d1099e29712cb
Claus-René Tacha <79410488+tcrnr1@users.noreply.github.com>
2026-06-12 11:34:18 +0200

    Refactor constants into a dictionary format

--- custom_components/renewable-energy-community/const.py
+++ custom_components/renewable-energy-community/const.py
@@ -1,27 +1,13 @@
-"""Constants for Renewable Energy Community."""
-
-# Base component constants
-NAME = "Renewable Energy Community"
-DOMAIN = "renewable_energy_community"
-VERSION = "0.1.0"
-
-# Platforms
-SENSOR = "sensor"
-PLATFORMS = [SENSOR]
-
-# Services
-SERVICE_IMPORT_REPORT = "import_report"
-
-# CSV Keys
-CSV_DATE_KEY = "Datum"
-CSV_TIME_KEY = "Uhrzeit"
-
-CSV_FEEDIN_GRID = "EinspeiseNetz"
-CSV_FEEDIN_COMMUNITY = "EinspeiseGemeinschaft"
-CSV_CONSUMPTION_GRID = "BezugNetz"
-CSV_CONSUMPTION_COMMUNITY = "BezugGemeinschaft"
-
-# Configuration and options
-DEFAULT_NAME = "SmartMeter"
-CONF_METER_POINT_NUMBER = "meter_point_number"
-CONF_NAME = "name"
+{
+  "domain": "renewable_energy_community",
+  "name": "Renewable Energy Community",
+  "documentation": "https://github.com/tcrnr1/ha-renewable-energy-community",
+  "iot_class": "calculated",
+  "issue_tracker": "https://github.com/tcrnr1/ha-renewable-energy-community/issues",
+  "version": "0.1.0",
+  "config_flow": true,
+  "codeowners": [
+    "@tcrnr1",
+    "@DarkC35"
+  ]
+}
